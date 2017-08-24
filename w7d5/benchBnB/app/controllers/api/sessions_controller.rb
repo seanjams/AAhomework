a99d4fd13399c39cc9cd04d10eba7fdb185cdb7c
Sean OReilly <seanvoreilly2@gmail.com>
2017-08-23 18:26:33 -0700

    finish curriculum

---
+++ w7d5/benchBnB/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      #render
+    else
+      #render errors
+    end
+  end
+
+  def destroy
+    logout!
+    render json: {}
+  end
+
+end
